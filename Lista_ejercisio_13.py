7987b20bd5f0e128403524565bfaad5cdab47131
josema43 <106554636+josema43@users.noreply.github.com>
2022-08-13 18:21:13 -0300

    Add files via upload

---
+++ Lista_ejercisio_13.py
@@ -0,0 +1,6 @@
+Perro=[13, 'Puppy', 12/12/2012,'Macho', 123]
+import mysql.connector
+animales= mysql.connector,connect(host="localhost",user="root", passws="3048", database="animales_veterinaria")
+print (animales)
+
+
